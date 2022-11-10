78107a230dc1bf27d08ce2ffa0b062b74d741d9e
Ale Algarra <alejandroalgarrad@gmail.com>
2022-11-10 16:02:22 +0100

    update

--- GExpenses/modelo/map.php
+++ GExpenses/modelo/map.php
@@ -51,13 +51,92 @@ class usuarios
     }
 }
 
-class Actividades {
+class Actividades
+{
 
     private int $a_id;
     private String $a_nom;
 
-    public function __construct($u_id, $u_username, $u_nombre, $u_apellidos, $u_correo, $u_password)
+    public function __construct($a_id, $a_nom)
     {
-
+        $this->a_id = $a_id;
+        $this->a_nom = $a_nom;
     }
+
+    public function getA_id()
+    {
+        return $this->a_id;
+    }
+
+    public function getA_nom()
+    {
+        return $this->a_nom;
+    }
+}
+
+class UsuariosActividades
+{
+
+    private int $a_idUsu;
+    private int $a_idAct;
+
+    public function __construct($a_idUsu, $a_idAct)
+    {
+        $this->a_idUsu = $a_idUsu;
+        $this->a_idAct = $a_idAct;
+    }
+
+    public function getA_idUsu()
+    {
+        return $this->a_idUsu;
+    }
+
+    public function getA_idAct()
+    {
+        return $this->a_idAct;
+    }
+}
+
+class Gastos {
+
+    private int $g_id;
+    private int $g_idUsu;
+    private int $g_idAct;
+    private int $g_precio;
+    private String $g_concepto;
+
+    public function __construct($g_id, $g_idUsu, $g_idAct, $g_precio, $g_concepto)
+    {
+        $this->g_id = $g_id;
+        $this->g_idUsu = $g_idUsu;
+        $this->g_idAct = $g_idAct;
+        $this->g_precio = $g_precio;
+        $this->g_concepto = $g_concepto;
+    }
+
+    public function getG_id()
+    {
+        return $this->g_id;
+    }
+
+    public function getG_idUsu()
+    {
+        return $this->g_idUsu;
+    }
+
+    public function getG_idAct()
+    {
+        return $this->g_idAct;
+    }
+
+    public function getG_precio()
+    {
+        return $this->g_precio;
+    }
+
+    public function getG_concepto()
+    {
+        return $this->g_concepto;
+    }
+
 }
