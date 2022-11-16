07fedecbdcf2f6b83d044f7eefdf71ffe356332c
alex <morales.luna.alex@alumnat.copernic.cat>
2022-11-16 21:39:37 +0100

    dialog más currado

--- GExpenses/vista/addActivityForm.php
+++ GExpenses/vista/addActivityForm.php
@@ -1,11 +1,23 @@
-<dialog id=addActivity>
-    <h5>Añadir actividad</h5>
-    <form method="dialog">
-        <label>Nombre:</label>
-        <input type="text" required>
-        <label>Moneda:</label>
-        <input type="text" required>
-        <label>Descripción:</label>
-        <textarea form="addActivity"></textarea>
-    </form>
+
+<dialog id=addActivityDialog class="dialogForm centered" open>
+    <div class="dialog-content">
+        <div class="modal-header">
+            <h4 class="titleDialog">Añadir actividad</h4>
+        </div>
+        <div class="dialog-content">
+            <form method="dialog" id="addActivity">
+
+                <label for="nombre" aria-placeholder="Viaje a Ibiza">Nombre:</label>
+                <input type="text" id="name" name="name" required>
+
+                <label for="moneda">Moneda:</label>
+                <input type="text" id="moneda" name="moneda" required>
+
+                <label for="descripcion">Descripción:</label>
+                <textarea form="addActivity" id="descripcion" name="descripcion" placeholder="Descripción de la actividad"></textarea>
+
+                <button type="submit" form="addActivity" value="Add" name="add">Añadir</button>
+            </form>
+        </div>
+    </div>
 </dialog>
