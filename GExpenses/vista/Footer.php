b1ca2e96bfd0d399768cc2eccf03462d8392a13b
Ale Algarra <alejandroalgarrad@gmail.com>
2022-11-16 19:28:45 +0100

    HEADER/FOOTER

---
+++ GExpenses/vista/Footer.php
@@ -0,0 +1,23 @@
+<footer>
+    <div class="footerBox">
+        <div class="partIzq">
+            <img id="logoFooter" src="../css/LOGO.png" alt="Logo pie de página">
+            <span>COPYRIGHT © 2022</span>
+        </div>
+    </div>
+
+    <div class="footerBox">
+        <div class="medio">
+            ENSURING MONEY PAYMENTS SINCE 2022
+        </div>
+    </div>
+
+    <div class="footerBox">
+        <div class="partDer">
+            <div>Max Freixa</div>
+            <div>Alex Morales</div>
+            <div>Ale Algarra</div>
+        </div>
+    </div>
+
+</footer>
---
+++ GExpenses/vista/Header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <div id="opcionesBox">
+            <img id="logo" src="../css/LOGO_ESTIRADO.png" alt="Logo cabecera">
+            <div class="opcion">Actividades</div>
+            <div class="opcion">Invitaciones</div>
+            <div class="opcion">Social</div>
+            <div class="opcion">Glosario</div>
+        </div>
+
+        <a id="usuario"> <?php $_SESSION['username'] = 'PerePou1234'; echo $_SESSION['username'] ?> </a>
+    </nav>
+</header>
