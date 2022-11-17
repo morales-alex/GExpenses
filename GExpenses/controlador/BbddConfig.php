c087d05868a34ded7b4b21df9376544eeae1b895
Ale Algarra <alejandroalgarrad@gmail.com>
2022-11-17 21:19:11 +0100

    mysql bbdd

--- GExpenses/controlador/BbddConfig.php
+++ GExpenses/controlador/BbddConfig.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $pdo = new PDO('sqlsrv:server=DESKTOP-8NG2OQ8\SQLEXPRESS;database=GExpenses_3P1');
+        $pdo = new PDO('mysql:dbname=GExpenses_3P1;host=127.0.0.1', 'root', '123456');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $ex) {
         echo 'Error de conexió: ' . $ex->getMessage();
