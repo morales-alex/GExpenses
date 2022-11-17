87d6e1c0f760a8c3a364b7a8d008e1789eb4a4f1
alex <morales.luna.alex@alumnat.copernic.cat>
2022-11-17 17:54:58 +0100

    ultimos cambios

--- GExpenses/vista/addActivityForm.php
+++ GExpenses/vista/addActivityForm.php
@@ -10,20 +10,20 @@
 
 <body>
     <dialog id=addActivityDialog class="dialogForm centered" open>
-        <h5>Añadir actividad</h5>
+        <div id="dialog-activityForm" class="dialog-header">
+            <h5>Añadir actividad</h5>
+            <span>x</span>
+        </div>
         <form method="post" action="actividad-action.php" id="addActivity">
             <label for="nombre">Nombre:</label>
             <input type="text" name="nombre" required>
-            <label for="moneda">Moneda:</label>
-            <input type="text" name="moneda" required>
+            <label for="moneda" >Moneda:</label>
+            <input type="text" name="moneda" placeholder="€,$.."required>
             <label for="descripcion">Descripción:</label>
-            <textarea for="descripcion" name="descripcion"></textarea>
-            <input type="submit" name="enviar" value="Enviar">
+            <textarea for="descripcion" name="descripcion" placeholder="Definicion de la actividad..."></textarea>
+            
         </form>
         <?php
-
-        session_start();
-
         if (isset($_SESSION["mensajeError"])) {
         ?>
             <div class="error-message"><?php echo $_SESSION["mensajeError"]; ?></div>
@@ -31,6 +31,11 @@
             unset($_SESSION["mensajeError"]);
         }
         ?>
+    <div class="dialog-footer">
+        <input type="button" class="boton-aceptar" value="Cerrar"></input>
+        <input type="submit" name="enviar" value="Enviar" class="boton-aceptar">
+        
+      </div>
     </dialog>
 </body>
 
