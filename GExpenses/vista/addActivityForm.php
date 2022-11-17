2097644e7a6c635e136ac92d86de7c8e37d94716
Max <sureda.sureda.max@copernic.cat>
2022-11-17 16:07:17 +0100

    Cambios formulario añadir actividades

--- GExpenses/vista/addActivityForm.php
+++ GExpenses/vista/addActivityForm.php
@@ -1,11 +1,37 @@
-<dialog id=addActivity>
-    <h5>Añadir actividad</h5>
-    <form method="dialog">
-        <label>Nombre:</label>
-        <input type="text" required>
-        <label>Moneda:</label>
-        <input type="text" required>
-        <label>Descripción:</label>
-        <textarea form="addActivity"></textarea>
-    </form>
-</dialog>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <dialog id=addActivity open>
+        <h5>Añadir actividad</h5>
+        <form method="post" action="actividad-action.php" id="addActivity">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" required>
+            <label for="moneda">Moneda:</label>
+            <input type="text" name="moneda" required>
+            <label for="descripcion">Descripción:</label>
+            <textarea for="descripcion" name="descripcion"></textarea>
+            <input type="submit" name="enviar" value="Enviar">
+        </form>
+        <?php
+
+        session_start();
+
+        if (isset($_SESSION["mensajeError"])) {
+        ?>
+            <div class="error-message"><?php echo $_SESSION["mensajeError"]; ?></div>
+        <?php
+            unset($_SESSION["mensajeError"]);
+        }
+        ?>
+    </dialog>
+</body>
+
+</html>
