ef3bccc540bd9f5bf780c1669da17934f2b7491b
alex <morales.luna.alex@alumnat.copernic.cat>
2022-11-23 20:44:01 +0100

    Vagrant con MariaDB

---
+++ vagrant/EstructuraBBDD.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE GExpenses_3P1;
+
+-- drop table gastos;drop table UsuariosActividades;drop table Usuarios;drop table Actividades;
+
+USE GExpenses_3P1;
+
+CREATE TABLE IF NOT EXISTS Usuarios ( 
+	u_id		int AUTO_INCREMENT, -- IDENTITY(1,1) = AUTO_INCREMENT, EMPIEZA EN 1 Y SUBE DE 1 EN 1
+	u_username	varchar(30) UNIQUE,
+	u_nombre	varchar(60),
+	u_apellidos	varchar(60),
+	u_correo	varchar(60) UNIQUE,
+	u_password	varchar(255) NOT NULL,
+	PRIMARY KEY(u_id)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Actividades (
+	a_id		int AUTO_INCREMENT,
+	a_nombre	varchar(30) NOT NULL,
+	a_moneda	char(1) NOT NULL,
+	a_descripcion varchar(130),
+    a_fecCreacion date,
+    a_fecUltMod date,
+	PRIMARY KEY(a_id)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS UsuariosActividades (
+	ua_idUsu		int,
+	ua_idAct		int,
+	PRIMARY KEY(ua_idUsu, ua_idAct),
+    FOREIGN KEY (ua_idUsu) REFERENCES Usuarios (u_id),
+    FOREIGN KEY (ua_idAct) REFERENCES Actividades (a_id)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Gastos (
+	g_id		int AUTO_INCREMENT,
+	g_idUsu		int, -- PERSONA QUE PAGA
+	g_idAct		int, -- ACTIVIDAD RELACIONADA
+	g_precio	int NOT NULL,
+	g_concepto	varchar(50),
+	PRIMARY KEY (g_id),
+	FOREIGN KEY (g_idUsu, g_idAct) REFERENCES UsuariosActividades (ua_idUsu, ua_idAct)
+) ENGINE=INNODB;
+
+
+INSERT INTO Usuarios (u_username, u_nombre, u_apellidos, u_correo, u_password) values('aalgarra', 'Alejandro', 'Algarra Delgado', 'algarra.delgado.alejandro@alumnat.copernic.cat', '$2y$10$o7.Xhj4uByDtF2gX0JRbouQcFSMV4TdghkS1QzmVcE/8KFliifFKK');
+INSERT INTO Usuarios (u_username, u_nombre, u_apellidos, u_correo, u_password) values('amorales', 'Alex', 'Morales Luna', 'morales.luna.alex@alumnat.copernic.cat', '$2y$10$o7.Xhj4uByDtF2gX0JRbouQcFSMV4TdghkS1QzmVcE/8KFliifFKK');
+INSERT INTO Usuarios (u_username, u_nombre, u_apellidos, u_correo, u_password) values('mfreixa', 'Max', 'Freixa Abcd', 'freixa.abcd.max@alumnat.copernic.cat', '$2y$10$o7.Xhj4uByDtF2gX0JRbouQcFSMV4TdghkS1QzmVcE/8KFliifFKK');
+
+INSERT INTO Actividades (a_nombre, a_moneda, a_descripcion, a_fecCreacion, a_fecUltMod) values('test', '€', 'Test Test TEst', '2022/11/01', '2022/11/18');
+INSERT INTO Actividades (a_nombre, a_moneda, a_descripcion, a_fecCreacion, a_fecUltMod) values('test2', '€', 'Test Test TEst', '2022/11/05', '2022/11/13');
+INSERT INTO Actividades (a_nombre, a_moneda, a_descripcion, a_fecCreacion, a_fecUltMod) values('test3', '€', 'Test Test TEst', '2022/11/08', '2022/11/15');
