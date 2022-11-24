2ccbf1cdbd8bcb78924791a635c1ebe14d7867de
alex <morales.luna.alex@alumnat.copernic.cat>
2022-11-24 15:51:27 +0100

    compruebaEmail funcion convertida archivo

--- GExpenses/vista/registro-action.php
+++ GExpenses/vista/registro-action.php
@@ -44,24 +44,4 @@ if (isset($_POST) && !compruebaEmail(htmlentities($_POST["email"]), $pdo)) {
     $_SESSION["mensajeError"] = "No funciono";
 }
 
-function compruebaEmail($correo, $pdo) {
-
-
-    try {
-        $sql = "SELECT u_correo FROM Usuarios where u_correo = :u_correo";
-        $stmt = $pdo->prepare($sql);
-
-        $stmt->bindParam(':u_correo', $correo);
-
-        $stmt->execute();
-        $datos = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if($datos == false) {
-            return false;
-        } else {
-            return true;
-        }
-    } catch (PDOException $ex) {
-        echo 'Error: ' . $ex->getMessage();
-    }
-}
+include 'compruebaEmail.php';
