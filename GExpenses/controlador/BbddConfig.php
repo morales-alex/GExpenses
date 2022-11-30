8830e42ea7c7ce351c262c4274f9a5313cd68722
alex <morales.luna.alex@alumnat.copernic.cat>
2022-11-30 18:37:19 +0100

    vagrant and BBDD

--- GExpenses/controlador/BbddConfig.php
+++ GExpenses/controlador/BbddConfig.php
@@ -1,7 +1,7 @@
 <?php
     try {
-        //*Vagrant*/ $pdo = new PDO('mysql:dbname=GExpenses_3P1;host=192.168.1.1', 'admin', 'mamadol');
-        /*Ale*/$pdo = new PDO('mysql:dbname=GExpenses_3P1;host=127.0.0.1', 'root', '123456');
+        /*Vagrant*/ $pdo = new PDO('mysql:dbname=GExpenses_3P1;host=192.168.50.4', 'admin', 'mamadol');
+        //*Ale*/$pdo = new PDO('mysql:dbname=GExpenses_3P1;host=127.0.0.1', 'root', '123456');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $ex) {
         echo 'Error de conexió: ' . $ex->getMessage();
