c8403ee586f0376f572c5522f0d1378e88976529
alex <morales.luna.alex@alumnat.copernic.cat>
2022-12-14 16:47:42 +0100

    factorización codigo para que sea en classes

--- site/script/validacionGastos.js
+++ site/script/validacionGastos.js
@@ -1,28 +1,44 @@
+class ValidacionCuantiaAvanzado{
+    cuantiaTotal;
+    totalSuma = 0;
+    constructor(cuantiaTotal){
+        this.cuantiaTotal = cuantiaTotal;
+        this.totalSuma = 0;
+    }
+    calcTotalSuma(inputs){
+        inputs.forEach(input =>{
+            this.totalSuma += parseInt(input.value);
+        });
+        return this.totalSuma;
+    }
+
+    logicaRetroaccionUsuario(){
+        if (totalSuma < cuantiaTotal) {
+            console.log(`Te quedan por asignar ${cuantiaTotal - totalSuma}`);
+            submit.disabled = true;
+        } else if (totalSuma > cuantiaTotal) {
+            console.log(`Te has pasado, sobran ${totalSuma - cuantiaTotal}`);
+            submit.disabled = true;
+        } else if(totalSuma === cuantiaTotal){
+            console.log(`Correcte (suma)${totalSuma} = (cuantiaTotal)${cuantiaTotal}`);
+            submit.disabled = false;
+        }
+    }
+    
+}
+
 const form = document.getElementById('addGastos');
 const inputs = Array.from(form.getElementsByClassName('pago'));
 const submit = document.getElementById('boton-aceptar-gastos');
-console.log(submit);
-console.log(inputs);
-const total = 10;
-let totalSuma = 0;
+const cuantiaTotal = document.getElementsByClassName('cuantia')[0].value;
+
+const ValidacionCuantiaAvanzado = new ValidacionCuantiaAvanzado(cuantiaTotal);
 
 inputs.forEach(input => {
     input.addEventListener('keyup', e => {
         totalSuma = 0;
-        inputs.forEach(input => {
-            totalSuma += parseInt(input.value);
-        });
-        if (totalSuma < total) {
-            console.log(`Te quedan por asignar ${total - totalSuma}`);
-            submit.disabled = true;
-        } else if (totalSuma > total) {
-            console.log(`Te has pasado, sobran ${totalSuma - total}`);
-            submit.disabled = true;
-        } else if(totalSuma === total){
-            console.log(`Correcte (suma)${totalSuma} = (total)${total}`);
-            submit.disabled = false;
-
-        }
+        ValidacionCuantiaAvanzado.calcTotalSuma(inputs);
+        logicaRetroaccionUsuario();
     });
 });
 
