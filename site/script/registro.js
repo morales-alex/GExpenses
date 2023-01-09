38c8a14442656cf93610a8f44e8a3b1b9e4b2479
Ale Algarra <alejandroalgarrad@gmail.com>
2023-01-09 18:19:52 +0100

    REGISTRO ARREGLADO.  - COMPRUEBA SI EXISTE CORREO Y USUARIO  - PONE MENSAJES DE ERROR EN REGISTRO SI EXISTE  - YA NO HAY TEXTOS SI SE EQUIVOCA USUARIO AHORA BORDE EN ROJO

--- site/script/registro.js
+++ site/script/registro.js
@@ -1,116 +1,128 @@
 // Verifica que el password es seguro, debe contener mayuscula, minuscula, simbolo y numero
 const validarPassword = () => {
-    var regexPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!#.$%&'@*+/=?])");
-    var comprovacionPassword = regexPassword.test(passwordInput.value);
+  var regexPassword = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!#.$%&'@*+/=?])"
+  );
+  var comprovacionPassword = regexPassword.test(passwordInput.value);
 
-    if (comprovacionPassword == true) {
-        console.log("Password vàlid");
-        passwordInput.nextElementSibling.classList.remove("mostrar");
-        return true;
-    } else {
-        passwordInput.nextElementSibling.classList.add("mostrar");
-    }
-}
+  if (comprovacionPassword) {
+    passwordInput.setAttribute("style", "border-color:black;");
+    return true;
+  } else {
+    passwordInput.setAttribute("style", "border-color:red;");
+  }
+};
 
 // Confirma que los dos password sean iguales
 const confirmarPassword = () => {
-    if (passwordInput.value == confirmarPasswordInput.value) {
-        console.log("Passowrd ok");
-        confirmarPasswordInput.nextElementSibling.classList.remove("mostrar");
-        return true;
-    } else {
-        console.log("Passowrd NO ok");
-        confirmarPasswordInput.nextElementSibling.classList.add("mostrar");
-    }
-}
+  var regexPassword = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!#.$%&'@*+/=?])"
+  );
+  var comprovacionPassword = regexPassword.test(confirmarPasswordInput.value);
+
+  if (
+    passwordInput.value == confirmarPasswordInput.value &&
+    comprovacionPassword
+  ) {
+    confirmarPasswordInput.setAttribute("style", "border-color:black;");
+    return true;
+  } else {
+    confirmarPasswordInput.setAttribute("style", "border-color:red;");
+  }
+};
 
 // Validar que el email és un email correcte
 function validarEmail() {
-    var mailformato = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (emailInput.value.match(mailformato)) {
-        console.log("Correo eletrónico valido");
-        emailInput.nextElementSibling.classList.remove("mostrar");
-        return true;
-    }
-    else {
-        console.log("Correo eletrónico NO valido");
-        emailInput.nextElementSibling.classList.add("mostrar");
-    }
+  mailExists.style.display = "none";
+
+  var mailformato = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailInput.value.match(mailformato)) {
+    emailInput.setAttribute("style", "border-color:black;");
+    return true;
+  } else {
+    emailInput.setAttribute("style", "border-color:red;");
+  }
 }
 
 // Validar que haya minimo dos apellidos y no contengan caracteres inválidos
 function validarApellidos() {
-    var apellidosformato = /^[a-zA-Z]+( [a-zA-Z]+)+$/;
-    if (apellidosInput.value.match(apellidosformato)) {
-        console.log("Apellidos validos");
-        apellidosInput.nextElementSibling.classList.remove("mostrar");
-        return true;
-    }
-    else {
-        console.log("Apellido NO validos");
-        apellidosInput.nextElementSibling.classList.add("mostrar");
-    }
+  var apellidosformato = /^[a-zA-Z]+( [a-zA-Z]+)+$/;
+  if (apellidosInput.value.match(apellidosformato)) {
+    apellidosInput.setAttribute("style", "border-color:black;");
+    return true;
+  } else {
+    apellidosInput.setAttribute("style", "border-color:red;");
+  }
 }
 
 // Validamos que haya un nombre sin caracteres invalidos
 function validarNombre() {
-    var nombreformato = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
-    if (nombreInput.value.match(nombreformato)) {
-        console.log("Nombre valido");
-        nombreInput.nextElementSibling.classList.remove("mostrar");
-        return true;
-    }
-    else {
-        console.log("Nombre NO valido");
-        nombreInput.nextElementSibling.classList.add("mostrar");
-    }
+  var nombreformato = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+  if (nombreInput.value.match(nombreformato)) {
+    nombreInput.setAttribute("style", "border-color:black;");
+    return true;
+  } else {
+    nombreInput.setAttribute("style", "border-color:red;");
+  }
 }
 
 // Validamos usuario
 function validarUsuario() {
-    var usuarioformato = /^[A-Za-z][A-Za-z0-9_]{4,30}$/;
-    if (usuarioInput.value.match(usuarioformato)) {
-        console.log("Ususario valido");
-        usuarioInput.nextElementSibling.classList.remove("mostrar");
-        return true;
-    }
-    else {
-        console.log("Usuario NO valido");
-        usuarioInput.nextElementSibling.classList.add("mostrar");
-    }
+  usernameExists.style.display = "none";
+
+  var usuarioformato = /^[A-Za-z][A-Za-z0-9_]{4,30}$/;
+  if (usuarioInput.value.match(usuarioformato)) {
+    usuarioInput.setAttribute("style", "border-color:black;");
+    return true;
+  } else {
+    usuarioInput.setAttribute("style", "border-color:red;");
+  }
 }
 
 function validarRegistro() {
-
-    if (validarNombre() && validarApellidos() && validarUsuario() && validarEmail() && validarPassword() && confirmarPassword()) {
-        console.log("Todos campos OK");
-        return true;
-    } else {
-        return false;
-    }
+  if (
+    validarNombre() &&
+    validarApellidos() &&
+    validarUsuario() &&
+    validarEmail() &&
+    validarPassword() &&
+    confirmarPassword()
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const passwordInput = document.querySelector("#password");
 const confirmarPasswordInput = document.querySelector("#password-confirm");
 const emailInput = document.querySelector("#email");
+
+const mailExists = document.querySelector(".mailExists");
+
 const apellidosInput = document.querySelector("#apellidos");
 const nombreInput = document.querySelector("#nombre");
 const usuarioInput = document.querySelector("#usuario");
+
+const usernameExists = document.querySelector(".usernameExists");
+
 const formulario = document.querySelector("#formulario");
 
-passwordInput.addEventListener('focusout', validarPassword);
-confirmarPasswordInput.addEventListener('focusout', confirmarPassword);
-emailInput.addEventListener('focusout', validarEmail);
-apellidosInput.addEventListener('focusout', validarApellidos);
-nombreInput.addEventListener('focusout', validarNombre);
-usuarioInput.addEventListener('focusout', validarUsuario);
-//formulario.addEventListener('submit', validarRegistro);
+passwordInput.addEventListener("focusout", validarPassword);
+confirmarPasswordInput.addEventListener("focusout", confirmarPassword);
+emailInput.addEventListener("focusout", validarEmail);
+apellidosInput.addEventListener("focusout", validarApellidos);
+nombreInput.addEventListener("focusout", validarNombre);
+usuarioInput.addEventListener("focusout", validarUsuario);
 
-formulario.addEventListener('submit', function (e) {
+formulario.addEventListener("submit", function (e) {
+
     e.preventDefault();
-    console.log(validarEmail());
 
-    if (validarRegistro()) {
-        formulario.submit();
-    }
+  if (validarRegistro()) {
+    formulario.submit();
+  } else {
+    usernameExists.style.display = "none";
+    mailExists.style.display = "none";
+  }
 });
--- site/vista/registro-action.php
+++ site/vista/registro-action.php
@@ -5,111 +5,158 @@ require '../controlador/compruebaEmail.php';
 require '../controlador/BbddConfig.php';
 require '../modelo/tablesMap.php';
 
-if (isset($_POST) && !compruebaEmail(htmlentities($_POST["email"]), $pdo)) {
-    $password_hasheado = htmlentities($_POST["password"]);
+if (isset($_POST)) {
 
-    $dades = [htmlentities($_POST["usuario"]), htmlentities($_POST["nombre"]), htmlentities($_POST["apellidos"]), htmlentities($_POST["email"]), password_hash($password_hasheado, PASSWORD_DEFAULT)];
+    $password_hasheado = htmlentities($_POST["password"]);
+    $usuarioCorreoExiste = false;
 
     try {
-        $sql = "INSERT INTO Usuarios (u_username, u_nombre, u_apellidos, u_correo, u_password) 
-                        VALUES (:u_username, :u_nombre, :u_apellidos, :u_correo, :u_password)";
+        $sql = "SELECT u_username 
+                    FROM Usuarios
+                WHERE u_username = :u_username";
         $stmt = $pdo->prepare($sql);
-
-        $stmt->bindParam(':u_username', $dades[0]);
-        $stmt->bindParam(':u_nombre', $dades[1]);
-        $stmt->bindParam(':u_apellidos', $dades[2]);
-        $stmt->bindParam(':u_correo', $dades[3]);
-        $stmt->bindParam(':u_password', $dades[4]);
-
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->beginTransaction();
-
+        $stmt->bindParam(':u_username', htmlentities($_POST["usuario"]));
         $stmt->execute();
-
-        $pdo->commit();
-
-
-        try {
-            $sql = "SELECT u_id
-                        FROM Usuarios
-                    WHERE u_username = :u_username;";
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindParam(':u_username', $dades[0]);
-
-            $stmt->execute();
-            $idUsuario = $stmt->fetch(PDO::FETCH_ASSOC);
-        } catch (PDOException $ex) {
-            echo 'Error: ' . $ex->getMessage();
-        }
-
-        session_unset();
-        $_SESSION['usuario'] = new usuarios($idUsuario['u_id'], $_POST["usuario"], $_POST["nombre"], $_POST["apellidos"], $_POST["email"]);
-
-        $_SESSION["mensajeError"] = "¡Enhorabuena! Usuario registrado correctamente!";
+        $usernameExists = $stmt->fetch(PDO::FETCH_ASSOC);
     } catch (PDOException $ex) {
-        $pdo->rollBack();
-        $_SESSION["mensajeError"] = 'Error: ' . $ex->getMessage();
-        header("Location: ./registro.php"); // INSERCION DE DATOS INCORRECTO
+        echo 'Error: ' . $ex->getMessage();
     }
 
-    // SI HI HA CAMPS DE INVITACIO I L'USUARI CORRESPON AMB EL TOKEM CONVIDAT REGISTRAL DIRECTAMENT A L'ACTIVITAT =============
-    if ($_POST['a-id'] !== '' && $_POST['invitacion'] !== '') {
 
-        $a_id = $_POST['a-id'];
-        $token = $_POST['invitacion'];
-        $correoUsuario = $_POST["email"];
-        $correoInvitado = null;
+    try {
+        $sql = "SELECT u_correo
+                    FROM Usuarios
+                WHERE u_correo = :u_correo";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':u_correo', htmlentities($_POST["email"]));
+        $stmt->execute();
+        $correoExists = $stmt->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $ex) {
+        echo 'Error: ' . $ex->getMessage();
+    }
 
-        // Consulta TOKEN existe
+    if ($usernameExists['u_username'] !== null && $correoExists['u_correo'] !== null) {
+
+        $_SESSION['err']['USERNAME_EXISTS'] = 'Ya existe ese usuario. Prueba otro!';
+        $_SESSION['err']['MAIL_EXISTS'] = 'Ya existe ese correo. Prueba otro!';
+
+        $usuarioCorreoExiste = true;
+    } else if ($usernameExists['u_username']) {
+
+        $_SESSION['err']['USERNAME_EXISTS'] = 'Ya existe ese usuario. Prueba otro!';
+        $usuarioCorreoExiste = true;
+    } else if ($correoExists['u_correo']) {
+
+        $_SESSION['err']['MAIL_EXISTS'] = 'Ya existe ese correo. Prueba otro!';
+        $usuarioCorreoExiste = true;
+    } else {
+
+
+        $dades = [htmlentities($_POST["usuario"]), htmlentities($_POST["nombre"]), htmlentities($_POST["apellidos"]), htmlentities($_POST["email"]), password_hash($password_hasheado, PASSWORD_DEFAULT)];
+
         try {
-            $sql = "SELECT i_correoUsuarioInvitado, u_id
+            $sql = "INSERT INTO Usuarios (u_username, u_nombre, u_apellidos, u_correo, u_password) 
+                        VALUES (:u_username, :u_nombre, :u_apellidos, :u_correo, :u_password)";
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->bindParam(':u_username', $dades[0]);
+            $stmt->bindParam(':u_nombre', $dades[1]);
+            $stmt->bindParam(':u_apellidos', $dades[2]);
+            $stmt->bindParam(':u_correo', $dades[3]);
+            $stmt->bindParam(':u_password', $dades[4]);
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->beginTransaction();
+
+            $stmt->execute();
+
+            $pdo->commit();
+
+
+            try {
+                $sql = "SELECT u_id
+                        FROM Usuarios
+                    WHERE u_username = :u_username;";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(':u_username', $dades[0]);
+
+                $stmt->execute();
+                $idUsuario = $stmt->fetch(PDO::FETCH_ASSOC);
+            } catch (PDOException $ex) {
+                echo 'Error: ' . $ex->getMessage();
+            }
+
+            session_unset();
+            $_SESSION['usuario'] = new usuarios($idUsuario['u_id'], $_POST["usuario"], $_POST["nombre"], $_POST["apellidos"], $_POST["email"]);
+
+            $_SESSION["mensajeError"] = "¡Enhorabuena! Usuario registrado correctamente!";
+        } catch (PDOException $ex) {
+            $pdo->rollBack();
+            $_SESSION["mensajeError"] = 'Error: ' . $ex->getMessage();
+            header("Location: ./registro.php"); // INSERCION DE DATOS INCORRECTO
+        }
+
+        // SI HI HA CAMPS DE INVITACIO I L'USUARI CORRESPON AMB EL TOKEM CONVIDAT REGISTRAL DIRECTAMENT A L'ACTIVITAT =============
+        if ($_POST['a-id'] !== '' && $_POST['invitacion'] !== '') {
+
+            $a_id = $_POST['a-id'];
+            $token = $_POST['invitacion'];
+            $correoUsuario = $_POST["email"];
+            $correoInvitado = null;
+
+            // Consulta TOKEN existe
+            try {
+                $sql = "SELECT i_correoUsuarioInvitado, u_id
                         FROM Invitaciones
                             INNER JOIN Usuarios ON Usuarios.u_correo = Invitaciones.i_correoUsuarioInvitado 
                     WHERE i_token = :i_token AND DATE_ADD(i_fecinv, INTERVAL +1 MINUTE) > sysdate()"; // LA INVITACION CADUCA EN 2 MINUTOS
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindParam(':i_token', $token);
-
-            $stmt->execute();
-            $correoInvitado = $stmt->fetch(PDO::FETCH_ASSOC);
-        } catch (PDOException $ex) {
-            echo 'Error: ' . $ex->getMessage();
-        }
-
-
-        // Si el token introducido coincide con el mail correspondiente en la base de datos lo inserta
-        if ($correoUsuario == $correoInvitado['i_correoUsuarioInvitado']) {
-
-            try {
-
-                $sql = "INSERT INTO UsuariosActividades (ua_idUsu, ua_idAct) 
-                            VALUES (:ua_idUsu, :ua_idAct)";
                 $stmt = $pdo->prepare($sql);
-
-                $stmt->bindParam(':ua_idUsu', $correoInvitado['u_id']);
-                $stmt->bindParam(':ua_idAct', $a_id);
-
-                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $pdo->beginTransaction();
+                $stmt->bindParam(':i_token', $token);
 
                 $stmt->execute();
-
-                $pdo->commit();
-
-                $_SESSION["mensajeError"] = $_SESSION["mensajeError"] . ' . Ha sido añadido a la actividad a la que has sido invitado!';
+                $correoInvitado = $stmt->fetch(PDO::FETCH_ASSOC);
             } catch (PDOException $ex) {
-                $pdo->rollBack();
-                $_SESSION["mensajeError"] = 'Error: ' . $ex->getMessage();
-                header("Location: ./registro.php"); // INSERCION DE DATOS INCORRECTO
+                echo 'Error: ' . $ex->getMessage();
+            }
 
+
+            // Si el token introducido coincide con el mail correspondiente en la base de datos lo inserta
+            if ($correoUsuario == $correoInvitado['i_correoUsuarioInvitado']) {
+
+                try {
+
+                    $sql = "INSERT INTO UsuariosActividades (ua_idUsu, ua_idAct) 
+                            VALUES (:ua_idUsu, :ua_idAct)";
+                    $stmt = $pdo->prepare($sql);
+
+                    $stmt->bindParam(':ua_idUsu', $correoInvitado['u_id']);
+                    $stmt->bindParam(':ua_idAct', $a_id);
+
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $pdo->beginTransaction();
+
+                    $stmt->execute();
+
+                    $pdo->commit();
+
+                    $_SESSION["mensajeError"] = $_SESSION["mensajeError"] . ' . Ha sido añadido a la actividad a la que has sido invitado!';
+                } catch (PDOException $ex) {
+                    $pdo->rollBack();
+                    $_SESSION["mensajeError"] = 'Error: ' . $ex->getMessage();
+                    header("Location: ./registro.php"); // INSERCION DE DATOS INCORRECTO
+
+                }
+                unset($_GET["invitacion"]);
+            } else {
+                $_SESSION["mensajeError"] = '¡Cuidado! Usuario registrado correctamente, pero lamentablemente no hemos podido añadirte a la actividad a la que has sido invitado.';
             }
             unset($_GET["invitacion"]);
-        } else {
-            $_SESSION["mensajeError"] = '¡Cuidado! Usuario registrado correctamente, pero lamentablemente no hemos podido añadirte a la actividad a la que has sido invitado.';
         }
-        unset($_GET["invitacion"]);
     }
 }
 
-
-
-header("Location: ./home.php");
+if ($usuarioCorreoExiste) {
+    header("Location: ./registro.php");
+} else {
+    header("Location: ./home.php");
+}
