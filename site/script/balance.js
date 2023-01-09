be787e9e65398dbc92e76a4c048b4a919340805c
Max <sureda.sureda.max@copernic.cat>
2023-01-09 17:54:16 +0100

    Cambios balance

--- site/script/balance.js
+++ site/script/balance.js
@@ -1,5 +1,6 @@
 const abrirFormulariBalance = document.querySelector(".balance");
 const balanceDialog = document.querySelector("#balanceDialog");
+const xBalance = document.querySelector("#xBalance");
 
 abrirFormulariBalance.addEventListener("click", function (e) {
     e.preventDefault();
@@ -9,4 +10,14 @@ abrirFormulariBalance.addEventListener("click", function (e) {
     nombreError.style.display = "none";
   
     balanceDialog.showModal();
+  });
+
+  xBalance.addEventListener("click", function (e) {
+    e.preventDefault();
+  
+    nombreActividad.value = "";
+  
+    nombreError.style.display = "none";
+  
+    balanceDialog.close("Dialogo cerrado");
   });
