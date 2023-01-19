f5ce1a262d7d78de5868ec36db273512439ed902
alex <morales.luna.alex@alumnat.copernic.cat>
2023-01-19 20:33:55 +0100

    bdd if not exists destroy

--- vagrant/installPhp.sh
+++ vagrant/installPhp.sh
@@ -2,7 +2,6 @@ echo 'Instalando PHP 8...'
 sudo apt-get install ca-certificates apt-transport-https software-properties-common -y
 sudo echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/sury-php.list
 sudo wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
-sudo apt-get update -y
 sudo apt-get -y install php8.0
 sudo apt-get -y install php-mysql
 sudo apt-get install php8.0-mysql
